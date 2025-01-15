13a9566a46f4b3a79a8f406c314d245169a6da88
Chandrakant Mishra <cm237575@gmail.com>
2025-01-15 23:54:45 +0530

    Your commit message

--- src/hooks/useLoadCSV.js
+++ src/hooks/useLoadCSV.js
@@ -23,6 +23,54 @@
 
 // export default useLoadCSV;
 //
+// import { useState, useEffect } from "react";
+// import { csv } from "d3-fetch";
+
+// const useLoadCSV = (csvPath) => {
+//   const [fetchedData, setFetchedData] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         setLoading(true);
+//         const data = await csv(csvPath);
+
+//         const processedData = data.map((item) => ({
+//           vin: item["VIN (1-10)"],
+//           county: item.County,
+//           city: item.City,
+//           state: item.State,
+//           postalCode: item["Postal Code"],
+//           modelYear: item["Model Year"],
+//           make: item.Make,
+//           model: item.Model,
+//           evType: item["Electric Vehicle Type"],
+//           electricRange: item["Electric Range"],
+//           cafvEligibility: item["Clean Alternative Fuel Vehicle (CAFV) Eligibility"],
+//           baseMsrp: item["Base MSRP"],
+//           electricUtility: item["Electric Utility"],
+//         }));
+
+//         setFetchedData(processedData);
+//       } catch (err) {
+//         setError("Error fetching or parsing CSV data.");
+//         console.error("Error fetching CSV data: ", err);
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+
+//     fetchData();
+//   }, [csvPath]);
+
+//   return { fetchedData, loading, error };
+// };
+
+// export default useLoadCSV;
+
+
 import { useState, useEffect } from "react";
 import { csv } from "d3-fetch";
 
